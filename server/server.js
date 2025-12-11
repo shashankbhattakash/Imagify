95a9726249bb2f8cf7af1e984d692bfdb5e62bc8
shashankbhattakash <akashbhatt5227@gmail.com>
2025-12-11 15:42:13 +0530

    added frontend deplpyed link

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 // --- Configure CORS explicitly ---
-const allowedOrigin = "https://imagify-kappa-nine.vercel.app/"; // set your frontend origin here
+const allowedOrigin = "https://imagify-kappa-nine.vercel.app"; // set your frontend origin here
 const corsOptions = {
   origin: allowedOrigin, // use '*' only for dev (not with credentials)
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
